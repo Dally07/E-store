e2e9878ded00881854e2f0befa3e41f92050cdf7
Dally <dallymaminomenjanahar@gmail.com>
2024-08-30 10:53:58 +0300

    config server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
-module.exports = {
-    user: 'postgres',
-    host : 'localhost',
-    database : 'computerStore_ecommerce',
-    password : 'noah1112',
-    port : 5432,
-};
+const app= require('./app');
+const { sequelize } = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync({
+    force: false
+}).then( () => {
+    console.log('base de donnees connecte...');
+    app.listen(PORT, () => {
+        console.log('le serveur est en marche sur le port ${PORT}');
+    });
+}).catch(err => {
+    console.error('impossible de connecter sur la base de donnees', err);
+});
