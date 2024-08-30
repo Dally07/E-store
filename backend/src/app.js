098bc077fdc090823befca1b72660e23cc9c0e71
Dally <dallymaminomenjanahar@gmail.com>
2024-08-30 23:46:23 +0300

    ajout du routes auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');require('dotenv').config();
 
 const app = express();
+const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
     res.send('Bonjour! API est en marche...');
