40462dcafa5fd2aa285c908d46ae14e07e198625
Dally <dallymaminomenjanahar@gmail.com>
2024-09-22 23:02:55 +0300

    commande statut et filtre

--- frontend/src/composant/commande.jsx
+++ frontend/src/composant/commande.jsx
@@ -12,7 +12,8 @@ const Commande = () => {
   const [ordersParPage, setcommandePerPage] = useState(10);
   const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [selectedQuantity, setSelectedQuantity] = useState('');
+ const [startDate, setStartDate] = useState('');
+ const [endDate, setEndDate] = useState('');
 
   const commande = [
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
@@ -41,31 +42,30 @@ const Commande = () => {
     {id: '25', date:'2024-09-15', statut: 'En traitement', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
-    {id: '25', date:'15/09/2024', statut: 'Encours', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
+    {id: '25', date:'2024-09-18', statut: 'Encours', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
     {id: '25', date:'2024-09-15', statut: 'Terminer', totale: '5.500.000 MGA', payement:'Mvola', nom: 'Sitraka', email: 'sitraka@gmail.com', telephone:'0345685471', produits:'link photo'},
 
 ];
 
- 
 
 
   
   // Fonction pour filtrer les produits selon le terme de recherche
   const filteredOrder = commande.filter(commande => {
     const categoryMatch = selectedCategory ? commande.statut === selectedCategory : true;
-    const quantityMatch = selectedQuantity ? 
-      (selectedQuantity === "Terminer" ? commande.statut === 'Terminer' :
-       selectedQuantity === "Encours" ? commande.statut === 'Encours' :
-       selectedQuantity === "En traitement" ? commande.statut === 'En traitemnt' : true) 
-      : true;
 
       const searchMatch = searchTerm ? 
       commande.statut.toLowerCase().includes(searchTerm.toLowerCase()) ||
       commande.nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
       commande.email.toLowerCase().includes(searchTerm.toLowerCase()) : true;
-    return categoryMatch && quantityMatch && searchMatch;
+
+      const commandeDate = new Date(commande.date);
+      const start = startDate ? new Date(startDate) : null
+      const end = endDate ? new Date(endDate): null
+      const datematch = (start && end) ? (commandeDate >= start && commandeDate <= end) : start ? (commandeDate >= start ) : end ? (commandeDate <= end) : true
+    return categoryMatch  && searchMatch && datematch;
   });
 
   
@@ -89,6 +89,16 @@ const Commande = () => {
     const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
     saveAs(blob, 'Commande_Report.xlsx');
   };
+
+
+  const getBackgrounColor = (statut) => {
+    switch (statut) {
+        case 'Terminer' : return 'bg-green-500';
+        case 'Encours' : return 'bg-yellow-500';
+        case 'En traitement' : return 'bg-gray-500';
+        default: return '';
+    }
+  }
 
   return (
     <div className="h-screen flex flex-col" style={{ backgroundColor: '#030C1B' }}>
@@ -178,7 +188,7 @@ const Commande = () => {
                   <tr key={index} className="hover:bg-gray-900 border-b">
                     <td className="py-2 px-4">{commande.id} <br />
                         {commande.date} <br />
-                        <span>{commande.statut}</span>
+                        <span className={`text-white rounded ${getBackgrounColor(commande.statut)}`}>{commande.statut}</span>
                     </td>
                     
                     <td className="py-2 px-4">  
@@ -192,7 +202,7 @@ const Commande = () => {
                       
                     </td>
                     <td className="py-2 px-4">  
-                      <span className="text-sm text-gray-500">{commande.produits}</span>
+                      <img className="text-sm text-gray-500" src={commande.produits} alt={commande.produits} />
                     </td>
                     
                     <td className="py-2 px-4">
@@ -240,31 +250,43 @@ const Commande = () => {
   <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
     <div className="bg-blue-900 p-6 rounded-lg" style={{ backgroundColor: '#041130' }}>
       <div className="flex justify-between mb-6">
-        <h2 className="text-white">Filtrer les Produits</h2>
+        <h2 className="text-white">Filtrer les Commandes</h2>
         <button onClick={() => setIsFilterModalOpen(false)} className="text-white">
           <FaWindowClose />
         </button>
       </div>
 
       <div>
-        <label className="text-white">Catégorie</label>
+        <label className="text-white">Statut</label>
         <select
           className="w-full p-2 rounded bg-gray-700 text-white"
           value={selectedCategory}
           onChange={(e) => setSelectedCategory(e.target.value)}
         >
+            <option value="">Tout</option>
           <option value="Terminer">Terminer</option>
           <option value="Encours">Encours</option>
           <option value="En traitement">En traitement</option>
         </select>
       </div>
 
-      <div className="mt-4">
-        <label className="text-white">Quantité</label>
+      <div className="flex mt-4">
+        <div>
+        <label className="text-white">Debut</label>
         <input type="date" 
         className="w-full p-2 rounded bg-gray-700 text-white"
-        value={selectedQuantity.date}
-        onChange={(e) => setSelectedQuantity(e.target.value)}/>
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}/>
+        </div>
+            
+            <div className='ml-2'>
+            <label className="text-white">Fin</label>
+        <input type="date" 
+        className="w-full p-2 rounded bg-gray-700 text-white"
+        value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}/>
+            </div>
+        
       </div>
 
       <button
