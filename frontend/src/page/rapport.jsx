557ff0165b4128c99a758fc2f171a4fa0ee2cb7e
Dally <dallymaminomenjanahar@gmail.com>
2024-10-25 20:55:10 +0300

    rapport journaliere, front

--- frontend/src/page/rapport.jsx
+++ frontend/src/page/rapport.jsx
@@ -1,72 +1,96 @@
-import React, { useState } from 'react';
-import { Bar, Pie } from 'react-chartjs-2';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Pie } from 'react-chartjs-2';
 import Header from '../composant/Header/header';
 import Sidebar from '../composant/sidebar/sidebar';
 import 'chart.js/auto';
+import { FaMoneyBillWave, FaShoppingCart, FaUser,FaUserCircle, FaStar } from 'react-icons/fa';
 
 const Rapport = () => {
   // État pour la date sélectionnée
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().substr(0, 10));
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString());
 
   // Données statiques pour simuler une réponse API
-  const rapportData = {
-    "2024-09-24": {
-      totalOrders: 35,
-      totalRevenue: 3500000,
-      revenues: [500000, 700000, 800000, 650000, 900000, 750000, 850000],
-      productsSold: [
-        { nom: 'PC', totalVendus: 30, photo: 'pc_image_url', configuration: 'Core i7, 16GB RAM' },
-        { nom: 'Imprimante', totalVendus: 25, photo: 'printer_image_url', configuration: '1200 dpi, 30ppm' },
-        { nom: 'Accessoire', totalVendus: 45, photo: 'accessory_image_url', configuration: 'Compatible PC et téléphone' },
-        { nom: 'Téléphone', totalVendus: 35, photo: 'phone_image_url', configuration: '128GB ROM, 6GB RAM' },
-      ],
-      paymentMethods: [45, 30, 25],
-      popularProduct: { nom: 'Accessoire', totalVendus: 45, photo: 'accessory_image_url', configuration: 'Compatible PC et téléphone' },
-      bestClient: { nom: 'John Doe', email: 'john.doe@example.com' }
-    }
-  };
-
-  const rapport = rapportData[selectedDate] || {
+  const [rapport, setRapport] = useState({
     totalOrders: 0,
     totalRevenue: 0,
+    totalClient: 0,
     revenues: [],
     productsSold: [],
     paymentMethods: [],
     popularProduct: null,
     bestClient: null
+  })
+
+  // Fonction pour récupérer les données du rapport
+  const fetchRapportData = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const [venteResponse, produitsResponse, paiementResponse, clientResponse] = await Promise.all([
+        axios.get(`http://localhost:3001/api/rapport-journalier?day=${selectedDate}`, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        }),
+        axios.get(`http://localhost:3001/api/rapport-journalier/statProd?day=${selectedDate}`, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        }),
+        axios.get(`http://localhost:3001/api/rapport-journalier/paiement?day=${selectedDate}`, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        }),
+        axios.get(`http://localhost:3001/api/rapport-journalier/topPoduct?day=${selectedDate}`, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        }),
+        
+      ]);
+
+      // Combiner les données reçues
+      const data = {
+        totalOrders: venteResponse.data.totalOrders || 0,
+        totalRevenue: venteResponse.data.totalRevenue || 0,
+        totalClient: venteResponse.data.totalClient || 0,
+        productsSold: produitsResponse.data.categorieCount || {}, // Ajustez si nécessaire
+        paymentMethods: paiementResponse.data.categorieCount || {},
+        popularProduct: produitsResponse.data.popularProduct || null, // Ajustez en fonction de votre réponse
+        bestClient: clientResponse.data.meilleur_client || null,
+      };
+
+      setRapport(data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données du rapport:', error);
+
+      setRapport({
+        totalOrders: 0,
+        totalRevenue: 0,
+        totalClient: 0,
+        productsSold: {},
+        paymentMethods: {},
+        popularProduct: null,
+        bestClient: null
+      });
+    }
   };
 
-  // Mise à jour des graphiques avec les données du rapport
-  const dataRevenus = {
-    labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
-    datasets: [
-      {
-        label: 'Revenu quotidien (MGA)',
-        data: rapport.revenues,
-        backgroundColor: 'rgba(0, 191, 255, 0.5)', // Bleu transparent
-        borderColor: 'rgba(0, 191, 255, 1)', // Bordure plus opaque
-        borderWidth: 1,
-      },
-    ],
-  };
+  useEffect(() => {
+    fetchRapportData();
+  }, [selectedDate]); 
+
 
   const dataProduits = {
-    labels: rapport.productsSold.map(p => p.nom),
+    labels: Object.keys(rapport.productsSold),
     datasets: [
       {
         label: 'Produits vendus',
-        data: rapport.productsSold.map(p => p.totalVendus),
+        data:  Object.values(rapport.productsSold),
         backgroundColor: [
-          'rgba(76, 175, 80, 0.5)', // Vert transparent
-          'rgba(255, 99, 132, 0.5)', // Rose transparent
-          'rgba(255, 206, 86, 0.5)', // Jaune transparent
-          'rgba(54, 162, 235, 0.5)', // Bleu transparent
+          'rgba(78, 178, 88, 0.8)', // Vert transparent
+          'rgba(237, 85, 101, 0.8)', // Rose transparent
+          'rgba(251, 189, 8, 0.8)', // Jaune transparent
+          'rgba(54, 162, 235, 0.8)', // Bleu transparent
         ],
         borderColor: [
-          'rgba(76, 175, 80, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(54, 162, 235, 1)',
+          'rgba(78, 178, 88, 1)',
+          'rgba(237, 85, 101, 1)',
+          'rgba(251, 189, 8, 1)',
+          'rgba(63, 138, 215, 1)',
         ],
         borderWidth: 1,
       },
@@ -74,11 +98,11 @@ const Rapport = () => {
   };
 
   const dataPaiement = {
-    labels: ['Carte', 'Espèce', 'Virement'],
+    labels: Object.keys(rapport.paymentMethods),
     datasets: [
       {
         label: 'Modes de paiement',
-        data: rapport.paymentMethods,
+        data:  Object.values(rapport.paymentMethods),
         backgroundColor: [
           'rgba(255, 99, 132, 0.6)', // Rouge doux
           'rgba(54, 162, 235, 0.6)', // Bleu doux
@@ -112,25 +136,58 @@ const Rapport = () => {
               onChange={e => setSelectedDate(e.target.value)}
             />
           </div>
+          
 
           <div className="grid grid-cols-3 gap-6 mb-6">
             {/* Total des commandes */}
-            <div className="p-6 rounded-lg" style={{ backgroundColor: '#041122' }}>
-              <h3 className="text-white font-bold">Total des Commandes</h3>
-              <p className="text-4xl text-white mt-4">{rapport.totalOrders}</p>
+            <div className="p-6 rounded-lg shadow-xl transition-shadow duration-300 bordered-b" style={{ backgroundColor: '#041122' }}>
+              <div className='flex justify-between items-center'>
+              <h3 className="text-white text-xl font-bold">Total des Commandes</h3>
+                    <FaShoppingCart className='text-3xl rounded-full text-red-700 mr-4 rounded-xl p-1' style={{backgroundColor: "#030C1B"}}/>
+              </div>
+              
+              <p className="text-xl text-gray-300 mt-2">{rapport.totalOrders}</p>
             </div>
 
             {/* Revenu total */}
-            <div className="p-6 rounded-lg" style={{ backgroundColor: '#041122' }}>
-              <h3 className="text-white font-bold">Revenu Total</h3>
-              <p className="text-4xl text-white mt-4">{rapport.totalRevenue} MGA</p>
+            <div className="p-6 rounded-lg shadow-xl transition-shadow duration-300 bordered-b" style={{ backgroundColor: '#041122' }}>
+              <div className='flex justify-between items-center'>
+              <h3 className="text-white text-xl font-bold">Revenu Total</h3>
+                <FaMoneyBillWave className='text-3xl rounded-full text-red-700 mr-4 rounded-xl p-1' style={{backgroundColor: "#030C1B"}}/>
+              </div>
+
+              <p className="text-xl text-gray-300 mt-2">{rapport.totalRevenue} MGA</p>
             </div>
             {/* client commande */}
-            <div className="p-6 rounded-lg" style={{ backgroundColor: '#041122' }}>
-              <h3 className="text-white font-bold">Revenu Total</h3>
-              <p className="text-4xl text-white mt-4">{rapport.totalRevenue} MGA</p>
+            <div className="p-6 rounded-lg shadow-xl transition-shadow duration-300 bordered-b" style={{ backgroundColor: '#041122' }}>
+              <div className='flex justify-between items-center'>
+              <h3 className="text-white text-xl font-bold">Client Commander</h3>
+              <FaUser className='text-3xl rounded-full text-red-700 mr-4 rounded-xl p-1' style={{backgroundColor: "#030C1B"}} />
+              </div>
+              
+              <p className="text-xl text-gray-300 mt-2">{rapport.totalClient}</p>
             </div>
           </div>
+          <div className="mb-6">
+  {/* Client le plus important avec style VIP */}
+  {rapport.bestClient && (
+    <div className="p-6 rounded-lg mt-6 shadow-lg flex items-center space-x-4" style={{ backgroundColor: '#041122' }}>
+      <FaUserCircle className="text-5xl text-yellow-300" />
+      <div>
+        <h3 className="text-yellow-300 font-bold mb-1">Client VIP</h3>
+        <p className="text-2xl text-white font-bold mb-1">{rapport.bestClient.nom}</p>
+        <p className="text-gray-400">{rapport.bestClient.email || "Aucune donnée trouvée"}</p>
+
+        {/* Section des étoiles */}
+        <div className="flex mt-2">
+          {[...Array(5)].map((_, i) => (
+            <FaStar key={i} className="text-yellow-500 text-lg animate-pulse mr-1" />
+          ))}
+        </div>
+      </div>
+    </div>
+  )}
+</div>
 
           <div className="grid grid-cols-2 gap-6">
             {/* Répartition des produits vendus */}
@@ -146,30 +203,7 @@ const Rapport = () => {
             </div>
           </div>
 
-          {/* Produit le plus vendu */}
-          {rapport.popularProduct && (
-            <div className="p-6 rounded-lg mt-6" style={{ backgroundColor: '#041122' }}>
-              <h3 className="text-white font-bold mb-4">Produit le plus vendu du jour</h3>
-              <div className="flex items-center">
-                <img src={rapport.popularProduct.photo} alt={rapport.popularProduct.nom} className="w-16 h-16 mr-4" />
-                <div>
-                  <p className="text-white font-bold">{rapport.popularProduct.nom}</p>
-                  <p className="text-gray-400">{rapport.popularProduct.configuration}</p>
-                </div>
-              </div>
-            </div>
-          )}
-
-          {/* Meilleur client */}
-          {rapport.bestClient && (
-            <div className="p-6 rounded-lg mt-6" style={{ backgroundColor: '#041122' }}>
-              <h3 className="text-white font-bold mb-4">Client le plus important</h3>
-              <div>
-                <p className="text-white font-bold">{rapport.bestClient.nom}</p>
-                <p className="text-gray-400">{rapport.bestClient.email}</p>
-              </div>
-            </div>
-          )}
+         
         </div>
       </div>
     </div>
